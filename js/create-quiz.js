fbaa74fdaa68e37ea0cb94bfed924ed1d33775a2
MarcusDantas <marvinsd@gmail.com>
2023-04-20 17:00:47 -0300

    feat: add validation for inputs of quiz creation

--- js/create-quiz.js
+++ js/create-quiz.js
@@ -1,6 +1,6 @@
 const createQuizScreen = document.querySelector(".create-quiz");
 let numberQuestions, numberLevels, quizImageUrl, quizTitle;
-const questions = [],
+let questions = [],
   levels = [];
 
 function toggleCreateQuiz() {
@@ -61,7 +61,51 @@ function sendQuiz() {
   quizCreated();
 }
 
+function isURL(str) {
+  const pattern = /^(https?:\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
+  return pattern.test(str);
+}
+
+function isHexColor(str) {
+  const pattern = /^#?([0-9A-F]{6})$/i;
+  return pattern.test(str);
+}
+
+function checkLevels() {
+  let wrongInput, easyLevel;
+  wrongInput = 0;
+  easyLevel = 0;
+
+  levels.forEach ((level) => {
+    if (level.title.length < 10) {
+      wrongInput ++;
+    };
+
+    if (!isURL(level.image)) {
+      wrongInput ++;
+    }
+
+    if (parseInt(level.minValue) < 0 || parseInt(level.minValue) > 100 || isNaN(parseInt(level.minValue))) {
+      wrongInput ++;
+    }
+
+    if (parseInt(level.minValue) === 0)
+      easyLevel ++;
+
+    if (level.text.length < 30) {
+      wrongInput ++;
+    }
+  })
+
+  if (easyLevel > 0 && wrongInput === 0 && levels.length === numberLevels) {
+    sendQuiz();
+    return;
+  }
+  alert("Preencha corretamente");
+}
+
 function saveLevels() {
+  levels = [];
   const getValue = (id) => {
     return document.getElementById(id) !== null
       ? document.getElementById(id).value
@@ -81,12 +125,8 @@ function saveLevels() {
         minValue,
       });
   }
-}
 
-async function saveQuiz() {
-  const levels = await saveLevels();
-  const send = levels;
-  sendQuiz(); 
+  checkLevels();
 }
 
 function toggleLevel(levelButton) {
@@ -101,43 +141,7 @@ function toggleLevel(levelButton) {
   level.nextElementSibling.classList.remove("hidden");
 }
 
-function saveQuestions() {
-  let answers = [];
-  const getValue = (id) => {
-    return document.getElementById(id) !== null
-      ? document.getElementById(id).value
-      : false;
-  };
-
-  for (let i = 1; i <= numberQuestions; i++) {
-    const title = getValue(`q-${i}`);
-    const color = getValue(`bg-${i}`);
-
-    if (title && color) {
-      for (let j = 1; j <= 4; j++) {
-        const text = getValue(`a-${j}${i}`);
-        const image = getValue(`Img-${j}${i}`);
-
-        if (text && image) {
-          answers.push({
-            text,
-            image,
-            isCorrectAnswer: j === 1,
-          });
-        }
-      }
-      questions.push({
-        title,
-        color,
-        answers,
-      });
-      answers = [];
-    }
-  }
-}
-
 function renderLevelsCreation() {
-  saveQuestions();
   let inputs = `
         <div class="inputs-container">
             <div class="question-banner selected-level">
@@ -173,9 +177,89 @@ function renderLevelsCreation() {
         <h2 class="create-quiz-title">Agora, decida os níveis!</h2>
         ${inputs}
         <div class="button-container">
-            <button onclick="saveQuiz()">Finalizar Quizz</button>
+            <button onclick="saveLevels()">Finalizar Quizz</button>
         </div>
     `;
+}
+
+function checkQuestions() {
+  let wrongInput = 0;
+  let correctAnswer = 0;
+
+  questions.forEach((item) => {
+    if (item.title.length < 20) {
+      wrongInput ++;
+    };
+    
+    if (!isHexColor(item.color)) {
+      wrongInput ++;
+    };
+
+    if (item.answers.length < 2) {
+      wrongInput ++;
+    }
+
+    item.answers.forEach((answer) => {
+      if (answer.text == "") {
+        wrongInput ++;
+      }
+
+      if (!isURL(answer.image)) {
+        wrongInput ++;
+      }
+
+      if (answer.isCorrectAnswer) {
+        correctAnswer ++;
+      }
+    })
+  })
+
+  if (correctAnswer === numberQuestions && wrongInput === 0 && questions.length === numberQuestions) {
+    renderLevelsCreation();
+    return;
+  }
+  console.log(wrongAnswer);
+  console.log(correctAnswer);
+  console.log(wrongInput);
+  alert("Preencha corretamente");
+}
+
+function saveQuestions() {
+  questions = [];
+  let answers = [];
+  const getValue = (id) => {
+    return document.getElementById(id) !== null
+      ? document.getElementById(id).value
+      : false;
+  };
+
+  for (let i = 1; i <= numberQuestions; i++) {
+    const title = getValue(`q-${i}`);
+    const color = getValue(`bg-${i}`);
+
+    if (title && color) {
+      for (let j = 1; j <= 4; j++) {
+        const text = getValue(`a-${j}${i}`);
+        const image = getValue(`Img-${j}${i}`);
+
+        if (text && image) {
+          answers.push({
+            text,
+            image,
+            isCorrectAnswer: j === 1,
+          });
+        }
+      }
+      questions.push({
+        title,
+        color,
+        answers,
+      });
+      answers = [];
+    }
+  }
+
+  checkQuestions();
 }
 
 function toggleQuestion(questionButton) {
@@ -262,17 +346,43 @@ function renderQuestionsCreation() {
         <h2 class="create-quiz-title">Crie suas perguntas</h2>
         ${inputs}
         <div class="button-container">
-            <button onclick="renderLevelsCreation()">Prosseguir pra criar níveis</button>
+            <button onclick="saveQuestions()">Prosseguir pra criar níveis</button>
         </div>
     `;
+}
+
+function checkMainInfo() {
+  let wrongInput = 0;
+
+  if (quizTitle.length < 20 || quizTitle.length > 65) {
+    wrongInput ++;
+  }
+
+  if (!isURL(quizImageUrl)) {
+    wrongInput ++;
+  }
+
+  if (numberQuestions < 3) {
+    wrongInput ++;
+  }
+
+  if (numberLevels < 2) {
+    wrongInput ++;
+  }
+  
+  if (wrongInput === 0) {
+    renderQuestionsCreation();
+    return;
+  }
+  console.log(wrongInput);
+  alert("Preencha corretamente");
 }
 
 function createQuestions() {
   quizTitle = createQuizScreen.querySelector("#quizTitleCreation").value;
   quizImageUrl = createQuizScreen.querySelector("#quizImageCreation").value;
-  numberQuestions = createQuizScreen.querySelector(
-    "#numberQuestionsCreation"
-  ).value;
-  numberLevels = createQuizScreen.querySelector("#numberLevelsCreation").value;
-  renderQuestionsCreation();
+  numberQuestions = parseInt(createQuizScreen.querySelector("#numberQuestionsCreation").value);
+  numberLevels = parseInt(createQuizScreen.querySelector("#numberLevelsCreation").value);
+  
+  checkMainInfo();
 }
